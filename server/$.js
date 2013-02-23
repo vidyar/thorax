c48cedcd1e696e47b67a09d7142c68f5b5dcc805
kpdecker <kpdecker@gmail.com>
2013-02-22 20:56:18 -0500

    Basic impl of server-side $

---
+++ server/$.js
@@ -0,0 +1,36 @@
+var Cheerio = require('cheerio');
+
+require('./cheerio-shim');
+
+// TODO : For testing run the zepto tests (that apply) against the output
+module.exports = exports = function(html, exec) {
+  var root = Cheerio.load(html);
+
+  function $(selector, context) {
+    // Special case the document instance
+    if (selector && selector.createElement) {
+      selector = root;
+    }
+    if (context && context.createElement) {
+      context = root;
+    }
+
+    return root(selector, context);
+  }
+
+  $.root = root;
+
+  // TODO : Proper user agent parsing
+  $.os = {};
+
+  $.ajax = function(options) {
+    // TODO : Implement
+    exec(function() {
+      options.success({}, 'success', {});
+      options.complete();
+    });
+  };
+  $.fn = Cheerio.prototype;
+
+  return $;
+};
