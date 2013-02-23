29cae13e83ff27131658508ca263ecded32ba8ae
kpdecker <kpdecker@gmail.com>
2013-02-23 10:11:26 -0400

    Update client context for full loading

--- server/client-context.js
+++ server/client-context.js
@@ -1,32 +1,110 @@
-var fs = require('fs'),
+var _ = require('underscore'),
+    fs = require('fs'),
+    jQuery = require('./$'),
+    path = require('path'),
     printf = require('printf'),
     sourceMap = require('./source-map'),
     vm = require('vm');
 
 const CONTEXT = 4;
 
-module.exports = exports = function(files) {
-  this.context = vm.createContext({
-    console: console,
-    window: {},
-    document: {},
-    $: function() {
-      return {
-        ready: function() {
-        }
-      };
-    }
-  });
-
-  files.forEach(function(content) {
-    var file = content.src || 'static';
-    content = content.src ? fs.readFileSync(content.src) : content;
-
+module.exports = exports = function(index) {
+  function exec(exec) {
     try {
-      vm.runInContext(content, this.context, file);
+      exec();
     } catch (err) {
       processError(err);
       throw err;
+    }
+  }
+  var $ = jQuery(fs.readFileSync(index), exec);
+
+  var window = {
+    $: $,
+
+    $server: true,
+    nextTick: function(callback) {
+      process.nextTick(function() { exec(callback); });
+    },
+
+    getComputedStyle: function() {},
+    navigator: {
+      userAgent: 'TODO : Whatever the user sent'
+    },
+
+    sessionStorage: {
+      getItem: function() {
+      }
+    },
+    localStorage: {
+      getItem: function() {
+      }
+    },
+    location: {
+      // TODO : Implement
+      href: 'http://localhost:8080/#home',
+      pathname: '/'
+    },
+
+    loadInContext: function(href, callback) {
+      if (href && context.lumbarLoadPrefix) {
+        href = path.relative(context.lumbarLoadPrefix, href);
+        href = path.resolve(path.dirname(index) + '/web', href);
+      }
+
+      exec(function() {
+        vm.runInContext(fs.readFileSync(href), context, href);
+      });
+
+      if (callback) {
+        window.nextTick(callback);
+      }
+    },
+
+    console: {
+      log: function() {
+        var args = _.map(arguments, function(arg) {
+          if (arg && arg.split) {
+            arg = stackContext(arg.split(/\n/g));
+          }
+          return arg;
+        });
+        console.log.apply(console, args);
+      },
+      error: function() {
+        window.console.log.apply(this, arguments);
+      }
+    },
+    document: {
+      get body() {
+        return $('body')[0];
+      },
+
+      querySelector: function(selector) {
+        return $(selector)[0];
+      },
+      createElement: function(tagName) {
+        return $('<' + tagName + '>');
+      }
+    }
+  };
+  window.jQuery = window.Zepto = window.$;
+  window.self = window.window = window;
+  window.document.defaultView = window;
+
+  var context = this.context = vm.createContext(window);
+
+  var files = $('script');
+  files.each(function() {
+    var text = $(this).text(),
+        external = $(this).attr('src');
+
+    if (external) {
+      window.loadInContext(external);
+    } else {
+      exec(function() {
+        vm.runInContext(text, context, text);
+      });
     }
   }, this);
 };
