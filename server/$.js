116636ecb59fb7843bec4ab858be85661e4457a1
kpdecker <kpdecker@gmail.com>
2013-02-24 10:47:12 -0400

    User Zeptos user agent sniffer

--- server/$.js
+++ server/$.js
@@ -1,5 +1,6 @@
 var ajax = require('./fruit-loops/ajax'),
     Cheerio = require('cheerio');
+    detect = require('./fruit-loops/detect');
 
 require('./cheerio-shim');
 
@@ -19,9 +20,7 @@ module.exports = exports = function(html, exec) {
     return root(selector, context);
   }
 
-  // TODO : Proper user agent parsing
-  $.os = {};
-
+  detect($, 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7');
 
   $.fn = Cheerio.prototype;
 
